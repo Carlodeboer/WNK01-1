67452524b5141109897ee8db146e963cf551f4d6
Carlodeboer <carlodb@hotmail.com>
2017-03-20 10:12:02 +0100

    lijnv1

--- Main/Lijnsensoren.ino
+++ Main/Lijnsensoren.ino
@@ -1,7 +1,9 @@
 long starttijd;
 long eindtijd;
 long verschil;
-bool temp;
+bool temp = false;
+
+int test = 0;
 
 
 void checkLijn() {
@@ -11,61 +13,73 @@ void checkLijn() {
   int proximityADC2 = analogRead(lijnSensor2);
   float proximityV2 = (float)proximityADC2 * 5.0 / 1023.0;
 
-if(proximityV1 > 2 && proximityV2 > 2){
-  starttijd = millis();
-  temp = true;
-  
-}
 
+  if (proximityV1 > 2 && proximityV2 > 2) {
+    starttijd = millis();
+    test = test+1;
+    temp = true;
 
-if(proximityV1 < 1 && proximityV2 < 1 && temp){
-  eindtijd = millis();
-  temp = false;
-  
-Serial.print("verschil: ");
-Serial.println(starttijd);
-Serial.println(eindtijd);
-Serial.println(eindtijd-starttijd);
-stopMotoren();
-}
-
-
-
-
-
-
- 
-//  if (proximityV1 > zwartWaarde && proximityV2 > zwartWaarde) {
-//    zwarteLijn = true;
-//  } else {
-//    zwarteLijn = false;
-//  }
-//
-//  if (zwarteLijn == false) {
-//    zwartTimer = 0;
-//  }
-//  else {
-//    zwartTimer ++;
-//  }
-//
-//    if (zwartTimer >= 1){
-//    Serial.println(zwartTimer);
-//
-//    if (zwartTimer == 10 && startBereikt == false) {
-//      Serial.println("Startlijn bereikt.");
-//      startBereikt = true;
-//      tienCentimeter();  
-//    }
-//    if(zwartTimer == 50) {
-//      Serial.println("Grenslijn bereikt.");
-//      stopMotoren();
-//    }
-//  }
-}
-
-  void stopMotoren() {
-    analogWrite(PWMMotor1, 0);
-    analogWrite(PWMMotor2, 0);
-    penOmhoog();
-    Serial.println("Motoren gestopt.");
   }
+
+
+  if (proximityV1 < 1 && proximityV2 < 1 && temp) {
+    eindtijd = millis();
+    temp = false;
+
+//    Serial.print("verschil: ");
+//    Serial.println(starttijd);
+//    Serial.println(eindtijd);
+//    Serial.println(eindtijd - starttijd);
+    Serial.println("counter value:");
+    Serial.println(test);
+
+    if(test > 400){
+    stopMotoren();
+    test = 0;
+    }
+    test = 0;
+  }
+
+
+
+
+
+
+
+  //  if (proximityV1 > zwartWaarde && proximityV2 > zwartWaarde) {
+  //    zwarteLijn = true;
+  //  } else {
+  //    zwarteLijn = false;
+  //  }
+  //
+  //  if (zwarteLijn == false) {
+  //    zwartTimer = 0;
+  //  }
+  //  else {
+  //    zwartTimer ++;
+  //  }
+  //
+  //    if (zwartTimer >= 1){
+  //    Serial.println(zwartTimer);
+  //
+  //    if (zwartTimer == 10 && startBereikt == false) {
+  //      Serial.println("Startlijn bereikt.");
+  //      startBereikt = true;
+  //      tienCentimeter();
+  //    }
+  //    if(zwartTimer == 50) {
+  //      Serial.println("Grenslijn bereikt.");
+  //      stopMotoren();
+  //    }
+  //  }
+}
+
+void stopMotoren() {
+  analogWrite(PWMMotor1, 0);
+  analogWrite(PWMMotor2, 0);
+  penOmhoog();
+  Serial.println("Motoren gestopt.");
+  Serial.println("Counter: ");
+  Serial.println(test);
+
+}
